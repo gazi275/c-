0ab98d2c3185f6620e84f0d5b5152214d85188d6
gazi275 <gaziazad270@gmail.com>
2025-12-26 15:59:12 +0600

    first commit

---
+++ print_array.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+    vec.push_back(2);
+    vec.push_back(3);
+    for(int val:vec)
+     {cout << val << endl;}
+}
